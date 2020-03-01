7ea9f33b08b7c02a9cceb4762b8ecef4d865672d
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-03-01 11:28:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -102,7 +102,7 @@ function draw(){
     
     snake.unshift(newHead);
 
-    if(snake.length > 25){
+    if(snake.length > 30){
         ctx.fillText("You Win!!!", 7*box,10 * box);
         clearInterval(game);
     }
